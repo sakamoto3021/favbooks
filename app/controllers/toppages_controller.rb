d7919299015bf28bcd99b32dfdb5890a4abfcc1f
ryutaro.sakamoto <sakamoto3021@yahoo.co.jp>
2018-10-27 03:27:30 +0000

    Search

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,9 +1,14 @@
 class ToppagesController < ApplicationController
   def index
-    if logged_in?
-      @posts=Post.order('created_at DESC').page(params[:page]).per(10)
+    if params[:search]
+      search = params[:search]
+      @posts = Post.where(['book_title LIKE ?', "%#{search}%"]).page(params[:page]).order('created_at DESC')
     else
-      @posts=Post.order('created_at DESC').page(params[:page]).per(3)
+      if logged_in?
+        @posts = Post.order('created_at DESC').page(params[:page]).per(10)
+      else
+        @posts=Post.order('created_at DESC').page(params[:page]).per(3)
+      end
     end
   end
 end
