638f2b3e9c6791810d62faf4ae422e4b279e03a2
ryutaro.sakamoto <sakamoto3021@yahoo.co.jp>
2018-11-18 13:52:40 +0000

    エラー解消

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,11 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
- 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '062dadd9bc5646b384044492ef222724.vfs.cloud9.us-east-2.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
