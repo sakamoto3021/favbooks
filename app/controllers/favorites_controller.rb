682049b8a5b704c6b4634e6dd7affac6230adba0
ryutaro.sakamoto <sakamoto3021@yahoo.co.jp>
2018-11-22 01:52:40 +0000

    投稿一覧にお気に入りボタン設置

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     post = Post.find(params[:like_id])
     current_user.like(post)
     flash[:success] = 'お気に入りに追加しました'
-    redirect_to post
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     post = Post.find(params[:like_id])
     current_user.unlike(post)
     flash[:success] = 'お気に入りを削除しました'
-    redirect_to post
+    redirect_back(fallback_location: root_path)
   end
 end
