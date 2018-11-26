b833ec5c94541e0d609b3caec7cb9169f388f48b
ryutaro.sakamoto <sakamoto3021@yahoo.co.jp>
2018-11-26 05:47:27 +0000

    お気に入りajax導入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,12 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    post = Post.find(params[:like_id])
-    current_user.like(post)
-    flash[:success] = 'お気に入りに追加しました'
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:like_id])
+    current_user.like(@post)
   end
 
   def destroy
-    post = Post.find(params[:like_id])
-    current_user.unlike(post)
-    flash[:success] = 'お気に入りを削除しました'
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:like_id])
+    current_user.unlike(@post)
   end
 end
